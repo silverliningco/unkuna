1597e8ef37710f56b849af1b1511dc7ce452d631
madays <maday.choque@gmail.com>
2021-02-12 11:12:04 -0500

    navbar.js cambio de clase a  nkn-nav-item

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -8,9 +8,9 @@ nkn.closeNavbar = function(e) {
 }
 
 
-// toggleNavbar opens or closes a responsive navbar by adding or removing CSS class nkn-open to the UL element with class nkn-navbar.
+// toggleNavbar opens or closes a responsive navbar by adding or removing CSS class nkn-open to the  element with class nkn-navbar.
 // toggle is the element which was clicked to trigger this function, ie. any element with data-nkn-role = navbar-toggle.
-// When opening a responsive navbar, we add click handlers to each LI element to close the navbar when clicked.
+// When opening a responsive navbar, we add click handlers to each LI element to close the navbar when clicked for LI elements that has class nkn-nav-item.
 nkn.toggleNavbar = function(toggle) {
     var myMenu = nkn.findParentElement(toggle, "nkn-navbar");
     if (myMenu) {
@@ -20,13 +20,12 @@ nkn.toggleNavbar = function(toggle) {
             myMenu.classList.add("nkn-open");
             var myLinks = myMenu.querySelectorAll("LI");
             for (var indx = 0; indx < myLinks.length; indx++) {
-                if (myLinks[indx].dataset.nknRole != "navbar-toggle") {
+                if (myLinks[indx].classList.contains("nkn-nav-item")) {
                     myLinks[indx].addEventListener("click", nkn.closeNavbar);
                 }
             }
         }
     }
 }
-
 
 
