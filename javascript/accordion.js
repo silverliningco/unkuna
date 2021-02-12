303bacc13674c3d1b516944580cca97d6bab41f9
madays <maday.choque@gmail.com>
2021-02-12 11:03:50 -0500

    accodion.js cambió el nombre de clase a nkn-accordion-item

--- javascript/accordion.js
+++ javascript/accordion.js
@@ -1,29 +1,28 @@
-// closeRemainingAccordion closes all sections in someAccordion except for someSection.
-// This is typically used if you want an accordion by design to open only a single section at a time.
-nkn.closeRemainingAccordion = function(someAccordion, someSection) {
-    var mySections = someAccordion.getElementsByClassName("nkn-accordion-section");
-    for (var indx = 0; indx < mySections.length; indx++) {
-        if (mySections[indx] != someSection) {
-            mySections[indx].classList.remove("nkn-open");
+// closeRemainingAccordion closes all items in someAccordion except for someItem.
+// This is typically used if you want an accordion by design to open only a single item at a time.
+nkn.closeRemainingAccordion = function(someAccordion, someItem) {
+    var myItems = someAccordion.getElementsByClassName("nkn-accordion-item");
+    for (var indx = 0; indx < myItems.length; indx++) {
+        if (myItems[indx] != someItem) {
+            myItems[indx].classList.remove("nkn-open");
         }
     }
-}
-
-
-// nkn.toggleAccordionSection opens or closes a section in an accordion.
-nkn.toggleAccordionSection = function(toggle) {
+ }
+  
+  
+ // nkn.toggleAccordionItem opens or closes an item in an accordion.
+ nkn.toggleAccordionItem = function(toggle) {
     var myAccordion = nkn.findParentElement(toggle, "nkn-accordion");
-    var mySection = nkn.findParentElement(toggle, "nkn-accordion-section");
-    if (mySection) {
-        if (mySection.classList.contains("nkn-open")) {
-            mySection.classList.remove("nkn-open");
+    var myItem = nkn.findParentElement(toggle, "nkn-accordion-item");
+    if (myItem) {
+        if (myItem.classList.contains("nkn-open")) {
+            myItem.classList.remove("nkn-open");
         } else {
-            mySection.classList.add("nkn-open");
+            myItem.classList.add("nkn-open");
             if (myAccordion.classList.contains("nkn-open-only-one")) {
-                this.closeRemainingAccordion(myAccordion, mySection);
+                this.closeRemainingAccordion(myAccordion, myItem);
             }
         }
     }
-}
-
-
+ }
+ 
