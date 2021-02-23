5e603639390c6f41f98c3fffa2df1f72606aebba
madays <maday.choque@gmail.com>
2021-02-23 17:36:00 -0500

    script para abrir y cerrar el hamburger menu

--- javascript/hamburger-menu.js
+++ javascript/hamburger-menu.js
@@ -1,18 +1,18 @@
-// closeMenu closes a "hamburger" menu by removing CSS class nkn-open.
+// closeHamburger closes a "hamburger" menu by removing CSS class nkn-open.
 // Clients usually do not need to call this directly, when opening a menu we automatically add "close" click handlers.
-nkn.closeMenu = function(e) {
-    var myMenu = nkn.findParentElement(e.target, "nkn-container-hamburger-menu");
+nkn.closeHamburger = function(e) {
+    var myMenu = nkn.findParentElement(e.target, "nkn-hamburger-menu");
     if (myMenu) {
         myMenu.classList.remove("nkn-open");
     }
 }
 
 
-// toggleMenu opens or closes a "hamburger" menu by adding or removing CSS class nkn-open to the UL element with class nkn-hamburger-menu.
-// toggle is the element which was clicked to trigger this function, ie. any element with data-nkn-role = menu-toggle.
+// toggleHamburger opens or closes a "hamburger" menu by adding or removing CSS class nkn-open to the element with class nkn-hamburger-menu.
+// toggle is the element which was clicked to trigger this function, ie. any element with data-nkn-role = hamburger-toggle.
 // When opening a "hamburger" menu, we add click handlers to each LI element to close the menu when clicked.
-nkn.toggleMenu = function(toggle) {
-    var myMenu = nkn.findParentElement(toggle, "nkn-container-hamburger-menu");
+nkn.toggleHamburger = function(toggle) {
+    var myMenu = nkn.findParentElement(toggle, "nkn-hamburger-menu");
     if (myMenu) {
         if (myMenu.classList.contains('nkn-open')) {
             myMenu.classList.remove("nkn-open");
@@ -20,9 +20,7 @@ nkn.toggleMenu = function(toggle) {
             myMenu.classList.add("nkn-open");
             var myLinks = myMenu.querySelectorAll("LI");
             for (var indx = 0; indx < myLinks.length; indx++) {
-                if (myLinks[indx].getAttribute("data-nkn-role") != "menu-toggle") {
-                    myLinks[indx].addEventListener("click", nkn.closeMenu);
-                }
+                myLinks[indx].addEventListener("click", nkn.closeMenu);
             }
         }
     }
